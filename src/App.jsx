d0a7fa810ba30608d327b7dc7cc119a5de608dc1
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-02-07 22:29:28 -0500

    feat:initial_project_app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Layout from "./components/Layout";
+import About from "./pages/About";
 function App (){
     return(
         <Router>
@@ -9,6 +10,7 @@ function App (){
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Home/>} />
                     <Route path="login" element={<Login/>} />
+                    <Route path="about" element={<About/>} />
                 </Route>
             </Routes>
         </Router>
